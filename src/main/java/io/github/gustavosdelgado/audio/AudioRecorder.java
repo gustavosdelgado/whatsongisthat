6e16b5a6587936b162b204cd8825d482edb04ac4
Gustavo Delgado <gustavodelgado@protonmail.com>
2023-04-11 18:46:53 -0300

    implemented peak frequencies calculation

--- src/main/java/io/github/gustavosdelgado/audio/AudioRecorder.java
+++ src/main/java/io/github/gustavosdelgado/audio/AudioRecorder.java
@@ -7,11 +7,14 @@ import javax.sound.sampled.TargetDataLine;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class AudioRecorder extends Thread {
 
     private static final int CHUNK_SIZE = 4096;
+
+    private static final int[] RANGE = new int[] { 40, 80, 120, 180, 300 };
+
+    private static final int FUZ_FACTOR = 2;
 
     private TargetDataLine line;
 
@@ -30,12 +33,14 @@ public class AudioRecorder extends Thread {
             System.err.println("Recording failure: " + e);
         }
 
-        performFFT(output);
+        Complex[][] results = performFFT(output);
+
+        long[][] peakFrequencies = calculatePeakFrequencies(results);
 
         System.out.println("Recording stopped.");
     }
 
-    private static void performFFT(ByteArrayOutputStream output) {
+    private static Complex[][] performFFT(ByteArrayOutputStream output) {
         byte[] audio = output.toByteArray();
         final int totalSize = audio.length;
         int totalNumberOfChunks = totalSize / CHUNK_SIZE; // 4kb size chunk
@@ -50,5 +55,40 @@ public class AudioRecorder extends Thread {
 
             results[i] = FFT.fft(complex); // Perform FFT analysis on each chunk:
         }
+
+        return results;
     }
+
+    private long[][] calculatePeakFrequencies(Complex[][] results) {
+        // SET 5 RANGES: 0~40, 40~80, 80~120, 120~180, 180~300
+        // FOR EACH chunk, TAKE THE FREQUENCY PEAK FOR EACH RANGE
+        long[][] peakFreq = new long[results.length][5];
+        double[][] highestMag = new double[results.length][5];
+
+        for (int i = 0; i < results.length; i++) { // for each chunk
+
+            for (int freq = 40; freq < 300 ; freq++) { // verify for each frequency
+                // Get the magnitude:
+                double mag = Math.log(results[i][freq].abs() + 1);
+                // Find out which range we are in:
+                int rangeIndex = getRangeIndex(freq);
+
+                // Save the highest magnitude and corresponding frequency:
+                if (mag > highestMag[i][rangeIndex]) {
+                    highestMag[i][rangeIndex] = mag;
+                    peakFreq[i][rangeIndex] = freq;
+                }
+            }
+        }
+
+        return peakFreq;
+    }
+
+    public int getRangeIndex(int freq) {
+        int i = 0;
+        while (RANGE[i] < freq)
+            i++;
+        return i;
+    }
+
 }
